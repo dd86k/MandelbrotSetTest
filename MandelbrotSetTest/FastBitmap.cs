a96bbe83ba0a0c8a59745506c89d10338ef867ef
DD~! <devddstuff@gmail.com>
2016-07-08 20:52:07 -0400

    Plans

--- MandelbrotSetTest/FastBitmap.cs
+++ MandelbrotSetTest/FastBitmap.cs
@@ -10,13 +10,14 @@ namespace MandelbrotSetTest
     /// </summary>
     /// <remarks>
     /// Something I took online but heavily modified for my taste and
-    /// to make it actually usable.
-    /// Original authors:
+    /// to make it actually usable. Like adding some comments, added a property,
+    /// etc.
+    /// Original post:
     /// http://www.vcskicks.com/fast-image-processing2.php
     /// </remarks>
     unsafe public class FastBitmap
     {
-        private struct PixelData
+        struct PixelData
         {
             public byte Alpha;
             public byte Red;
@@ -42,6 +43,12 @@ namespace MandelbrotSetTest
         public FastBitmap(Bitmap inputBitmap)
         {
             workingBitmap = inputBitmap;
+            /*
+            bitmapData = null;
+            pixelData = null;
+            pBase = null;
+            width = 0;
+            */
         }
 
         /**
@@ -51,34 +58,52 @@ namespace MandelbrotSetTest
         public void LockImage()
         {
             Rectangle bounds = new Rectangle(Point.Empty, workingBitmap.Size);
-
             width = bounds.Width * sizeof(PixelData);
-            if (width % 4 != 0) width = 4 * (width / 4 + 1);
 
-            //Lock Image
-            bitmapData = workingBitmap.LockBits(bounds, ImageLockMode.ReadWrite, PixelFormat.Format32bppArgb);
+            if (width % 4 != 0)
+                width = 4 * (width / 4 + 1);
+
+            bitmapData =
+                workingBitmap.LockBits(bounds, ImageLockMode.ReadWrite, workingBitmap.PixelFormat);
             pBase = (byte*)bitmapData.Scan0.ToPointer();
         }
 
         public Color GetPixel(int x, int y)
         {
             pixelData = (PixelData*)(pBase + y * width + x * sizeof(PixelData));
-            return Color.FromArgb(pixelData->Alpha, pixelData->Red, pixelData->Green, pixelData->Blue);
+            return Color.FromArgb(pixelData->Alpha,
+                                  pixelData->Red,
+                                  pixelData->Green,
+                                  pixelData->Blue);
         }
 
         public Color GetPixelNext()
         {
             pixelData++;
-            return Color.FromArgb(pixelData->Alpha, pixelData->Red, pixelData->Green, pixelData->Blue);
+            return Color.FromArgb(pixelData->Alpha,
+                                  pixelData->Red,
+                                  pixelData->Green,
+                                  pixelData->Blue);
         }
 
-        public void SetPixel(long x, int y, Color color)
+        public void SetPixel(int x, int y, Color color)
         {
             PixelData* data = (PixelData*)(pBase + y * width + x * sizeof(PixelData));
+            // PixelFormat.Format32bppArgb
             data->Alpha = color.A;
             data->Red = color.R;
             data->Green = color.G;
             data->Blue = color.B;
+        }
+
+        public void SetPixel(int x, int y, byte a, byte r, byte b, byte g)
+        {
+            PixelData* data = (PixelData*)(pBase + y * width + x * sizeof(PixelData));
+            // PixelFormat.Format32bppArgb
+            data->Alpha = a;
+            data->Red = r;
+            data->Green = g;
+            data->Blue = b;
         }
 
         public void UnlockImage()
--- MandelbrotSetTest/MandelbrotControl.cs
+++ MandelbrotSetTest/MandelbrotControl.cs
@@ -1,22 +1,39 @@
-﻿// Usage: S<SolutionNumber>
-#define S2
-
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 using System.Drawing;
 using System.Numerics;
-#if S3
-using static System.Threading.Tasks.Parallel;
-#endif
-using System.Drawing.Imaging;
+using System.Threading.Tasks;
+using System;
 
 namespace MandelbrotSetTest
 {
+/*
+ * Mouse event
+    mx1 = e.X
+    my1 = e.Y
+
+    double scaleX, scaleY;
+	scaleX = (cx1 - cx0) / (double )panelMain.Width;
+	scaleY = (cy1 - cy0) / (double)panelMain.Height;
+	cx1 = (double )mx1*scaleX + cx0;
+	cy1 = (double)my1*scaleY + cy0;
+	cx0 = (double)mx0 * scaleX + cx0;
+	cy0 = (double)my0 * scaleY + cy0;
+*/
+
     class MandelbrotControl : Control
     {
         public MandelbrotControl()
         {
             BackColor = Color.Black;
+            SetStyle(ControlStyles.UserPaint |
+                     ControlStyles.OptimizedDoubleBuffer |
+                     ControlStyles.AllPaintingInWmPaint, true);
         }
+
+        public double CX0 = -2.0;
+        public double CX1 = 3.0;
+        public double CY0 = -1.0;
+        public double CY1 = 2.0;
 
         public void Render()
         {
@@ -40,122 +57,44 @@ namespace MandelbrotSetTest
             double h = Height;
             Bitmap b = new Bitmap((int)w, (int)h);
             // Maximum iterations
-            const int m = 50;
+            const int m = 30;
             Complex z = new Complex();
             Complex c = new Complex();
-            
-#if S1
-            /**
-             * #1 - Supposedly fast, works when PixelFormat is Format24bppRgb, but is squished (X)
-             * Taken from an online post 
-             */
 
-            BitmapData bitmapData =
-                b.LockBits(new Rectangle(0,0,(int)w,(int)h), ImageLockMode.WriteOnly, b.PixelFormat);
-
-            int bytesPerPixel = Image.GetPixelFormatSize(b.PixelFormat) / 8;
-            int heightInPixels = bitmapData.Height;
-            int widthInBytes = bitmapData.Width * bytesPerPixel;
-
-            byte* PtrFirstPixel = (byte*)bitmapData.Scan0;
-
-            for (int y = 0; y < h; y++)
-            {
-                byte* currentLine = PtrFirstPixel + (y * bitmapData.Stride);
-                for (int x = 0; x < widthInBytes; x += bytesPerPixel)
-                {
-                    z = 0;
-                    c = new Complex(-2.0 + x / w * 3, -1 + y / h * 2);
-                    int i = 0;
-
-                    while (i < m)
-                    {
-                        z = Complex.Pow(z, 2) + c;
-
-                        if (Complex.Abs(z) > 2)
-                            break;
-
-                        ++i;
-                    }
-
-                    Color g = MapColor((double)i / m);
-
-                    currentLine[x + 2] = g.R;
-                    currentLine[x + 1] = g.G;
-                    currentLine[x] = g.B;
-                }
-            }
-            b.UnlockBits(bitmapData);
-            e.Graphics.DrawImageUnscaled(b, 0, 0);
-            b.Dispose();
-#endif
-#if S2
-            /**
-             * #2 FastBitmap, works best for now
-             */
-
-            FastBitmap f = new FastBitmap(b);
+            FastBitmap f = new FastBitmap(b); // Well, at least faster than Bitmap
             f.LockImage();
-            for (int y = 0; y < h; y++)
+
+            // Tried to not block the main thread.
+            Invoke(new Action(() =>
             {
-                for (int x = 0; x < w; x++)
+                for (int y = 0; y < (int)h; ++y)
                 {
-                    z = 0;
-                    c = new Complex(-2.0 + x / w * 3, -1 + y / h * 2);
-                    int i = 0;
-
-                    while (i < m)
+                    for (int x = 0; x < w; x++)
                     {
-                        z = Complex.Pow(z, 2) + c;
+                        z = 0;
+                        c = new Complex(CX0 + x / w * CX1, CY0 + y / h * CY1); // 3.0, 2.0
+                        int i = 0;
 
-                        if (Complex.Abs(z) > 2)
-                            break;
+                        while (i < m)
+                        {
+                            z = Complex.Pow(z, 2) + c;
 
-                        ++i;
+                            if (Complex.Abs(z) > 2)
+                                break;
+
+                            ++i;
+                        }
+
+                        f.SetPixel(x, y, MapColor((double)i / m));
                     }
-
-                    f.SetPixel(x, y, MapColor((double)i / m));
                 }
-            }
+            }));
+
             f.UnlockImage();
             e.Graphics.DrawImageUnscaled(f.Image, 0, 0);
             f.Image.Dispose();
-#endif
-#if S3
-            /**
-             * #3 FastBitmap + Parrarel, fastest, broken (colored TV static)
-             */
 
-            FastBitmap f = new FastBitmap(b);
-            f.LockImage();
-            For(0, (int)h, y =>
-            {
-                for (int x = 0; x < w; x++)
-                {
-                    z = 0;
-                    c = new Complex(-2.0 + x / w * 3, -1 + y / h * 2);
-                    int i = 0;
-
-                    while (i < m)
-                    {
-                        z = Complex.Pow(z, 2) + c;
-
-                        if (Complex.Abs(z) > 2)
-                            break;
-
-                        ++i;
-                    }
-
-                    f.SetPixel(x, y, MapColor((double)i / m));
-                }
-            });
-            f.UnlockImage();
-            e.Graphics.DrawImageUnscaled(f.Image, 0, 0);
-            f.Image.Dispose();
-#endif
-
-            e.Graphics.DrawRectangle(new Pen(Color.Red), 0, 0, (int)w - 1, (int)h - 1);
-            ResumeLayout();
+            ResumeLayout(true);
         }
     }
 }
